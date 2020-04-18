fe860cd425c78834666698e9a2b1e1fddc711dc5
contrecc <colinccontreary@gmail.com>
2020-04-18 15:42:01 -0700

    Add navigation to MealDetail screen using react-navigation

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { StyleSheet, Text, Button, View } from "react-native";
 
+import { MEALS } from '../data/dummy-data';
+
 const MealDetailScreen = props => {
+  const mealId = props.navigation.getParam('mealId');
+  const selectedId = MEALS.find(meal => meal.id === mealId);
   return (
     <View style={styles.screen}>
-      <Text>The Meal Detail Screen</Text>
+      <Text>{selectedId.title}</Text>
       <Button
         title="Go To Categories"
         onPress={() => props.navigation.popToTop()}
@@ -12,6 +16,14 @@ const MealDetailScreen = props => {
     </View>
   );
 };
+
+MealDetailScreen.navigationOptions = (navigationData) => {
+  const mealId = navigationData.navigation.getParam('mealId');
+  const selectedId = MEALS.find(meal => meal.id === mealId);
+  return {
+    headerTitle: selectedId.title
+  };
+}
 
 export default MealDetailScreen;
 
